21f1ff567579fda3cf7187900355996f277b3f0d
Joseph Goldstone <joseph.goldstone@mac.com>
2026-08-09 12:35:29 +0200

    Overview document initial commit
    Signed-off-by: Joseph Goldstone <joseph.goldstone@mac.com>

---
+++ Camera_and_lens_metadata.kt
@@ -0,0 +1,41 @@
+@document
+:title Camera and lens metadata (CLM) consistency, integrity and provenance
+:author Joseph Goldstone
+:date @date@
+:leading 1
+:bottom Joseph Goldstone --- Camera and lens metadata consistency, integrity and provenance --- @date@
+:point_size 12
+:text
+
+@s1 Goals s1@
+
+Describe CLM, why it is useful, and how to get it to artists
+
+@ul
+  existing CLM
+| existing and anticipated mechanisms for generating CLM
+| how CLM is carried
+| how CLM is lost
+| wasted time and effort that CLM could remedy
+ul@
+
+Catalog business models that have tried and failed to create a market for CLM
+
+@ul business models for a CLM ecosystem
+  lens-vendor models
+| camera-vendor models
+| third-party device models
+| diagnostic tool models
+ul@
+
+Sketch a case for an Academy Software Foundation (ASWF)
+
+
+@s1 Primary sources s1@
+
+@link https://pub.smpte.org/doc/rdd18/20210701-pub/ :title "Acquisition Metadata Sets for Video Camera Parameters" link@
+
+@link https://pub.smpte.org/doc/rdd55/20220505-pub/ :title "Material Exchange Format — Carriage of ARRI Camera System Metadata" link@
+
+document@
+
