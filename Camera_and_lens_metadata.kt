dc238849f9a5e30d206b03bdfd6ff6e8b841afe7
Joseph Goldstone <joseph.goldstone@mac.com>
2026-08-09 13:48:39 +0200

    Move ASWF content into beginnings of own section and add reduced-size summary of it
    Signed-off-by: Joseph Goldstone <joseph.goldstone@mac.com>

--- Camera_and_lens_metadata.kt
+++ Camera_and_lens_metadata.kt
@@ -1,41 +1,62 @@
 @document
-:title Camera and lens metadata (CLM) consistency, integrity and provenance
+:title Camera and lens metadata (CALM) consistency, integrity and provenance
 :author Joseph Goldstone
 :date @date@
 :leading 1
-:bottom Joseph Goldstone --- Camera and lens metadata consistency, integrity and provenance --- @date@
+:bottom Joseph Goldstone --- CALM consistency, integrity and provenance --- @date@
 :point_size 12
 :text
 
 @s1 Goals s1@
 
-Describe CLM, why it is useful, and how to get it to artists
+Describe CALM, why it is useful, and how to get it to artists
 
 @ul
-  existing CLM
-| existing and anticipated mechanisms for generating CLM
-| how CLM is carried
-| how CLM is lost
-| wasted time and effort that CLM could remedy
+  existing CALM
+| existing and anticipated mechanisms for generating CALM
+| how CALM is carried
+| how CALM is lost
+| wasted time and effort that CALM could remedy
 ul@
 
-Catalog business models that have tried and failed to create a market for CLM
+Catalog business models that have tried and failed to create a CALM ecosystem
 
-@ul business models for a CLM ecosystem
+@ul
   lens-vendor models
 | camera-vendor models
 | third-party device models
 | diagnostic tool models
 ul@
 
-Sketch a case for an Academy Software Foundation (ASWF)
+Sketch a case for an Academy Software Foundation (ASWF) CALM project
+@ul
+  recent open-sourcing of key materials
+| ASWF existing relationships to DCC companies
+| ASWF continuous integration infrastructure
+| ASWF experience with competing collaborators
+| ASWF artist recognition
+ul@
+
+@s1 Leveraging ASWF strengths for genuine @i artist i@ benefit s1@
+
+@ul
+  an open-source CALM project can now reference freely-available standards.
+| @ul
+    SMPTE standards are freely available since summer 2026, are structured through a combination of international normative standards (W3C) and open-sourced, GitHub-centric SMPTE substrate.
+  | ITU has always had freely available standards.
+  ul@
+| ISO standards are not freely available @i generally i@ but the parts typically referenced by SMPTE and ITU standards, the Terms and Definitions components, @i are i@ freely available.
+| The ASWF has links to CALM consumers (DCC applications, through their vendors) that SMPTE does not.
+| The ASWF has continuous integration (CI) infrastructure that SMPTE does not.
+| The ASWF has brand recognition in the community of CG artists that neither SMPTE, the ITU, or individual camera and lens manufacturers possess.
+ul@
 
 
 @s1 Primary sources s1@
 
-@link https://pub.smpte.org/doc/rdd18/20210701-pub/ :title "Acquisition Metadata Sets for Video Camera Parameters" link@
+@link https://pub.smpte.org/doc/rdd18/20210701-pub/ :text Acquisition Metadata Sets for Video Camera Parameters link@
 
-@link https://pub.smpte.org/doc/rdd55/20220505-pub/ :title "Material Exchange Format — Carriage of ARRI Camera System Metadata" link@
+@link https://pub.smpte.org/doc/rdd55/20220505-pub/ :text Material Exchange Format — Carriage of ARRI Camera System Metadata link@
 
 document@
 
